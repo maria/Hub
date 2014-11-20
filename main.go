ff32000fb3def9a0856c8fe0e86607ce4ca78846
Marius Cotofana <marius.coto@gmail.com>
2014-11-21 00:50:57 +0200

    Add connection to local mongodb; run a test insert

--- main.go
+++ main.go
@@ -2,12 +2,38 @@ package main
 
 import (
   "github.com/gorilla/mux"
+  "gopkg.in/mgo.v2"
+  "gopkg.in/mgo.v2/bson"
   "log"
   "net/http"
+  "os"
 )
+
+const MONGO_URL = "localhost:27017"
+
+type user struct {
+  Id        bson.ObjectId  `bson:"_id"`
+  Username  string         `bson:"username"`
+}
 
 
 func main() {
+
+  sess, err := mgo.Dial(MONGO_URL)
+  if err != nil {
+    log.Fatalln("Cannot connect to mongo.")
+    os.Exit(1)
+  }
+  defer sess.Close()
+
+  collection := sess.DB("test").C("foo")
+  doc := user{Id: bson.NewObjectId(), Username: "root"}
+  err = collection.Insert(doc)
+  if err != nil {
+    log.Println("No insert.")
+    os.Exit(1)
+  }
+
   mux := mux.NewRouter()
   mux.HandleFunc("/", index)
 
