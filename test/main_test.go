94a7fbb8a06631599121b3814579afb1f6c9ea74
Maria Nita <maria.nita.dn@gmail.com>
2014-11-23 22:02:07 +0100

    Add directory with tests

--- test/main_test.go
+++ test/main_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	path := "main.go"
+	path := "../main.go"
 	_, err := ioutil.ReadFile(path)
 	if err != nil {
 		t.Fatalf("Unable to read file: %v", err)
